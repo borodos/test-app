3657aab1fddd7192668d3e36eff2b50ac1fbf0ca
borodos <stepllerrus78@gmail.com>
2022-04-25 00:00:16 +0300

    123

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./css/link.css";
+
+function setActive({ isActive, className }) {
+	return isActive ? "active" : "link";
+}
 
 function NavBar() {
 	return (
 		<Navbar bg="dark" variant="dark">
 			<Container>
 				<Navbar.Brand href="/">FoodSher</Navbar.Brand>
-				<Nav className="ml-auto">
-					<Nav.Link href="/search">Найти еду</Nav.Link>
-					<Nav.Link href="/basket">Мои заказы</Nav.Link>
-					<Nav.Link href="/profile">Профиль</Nav.Link>
+				<Nav className="ml-auto nav">
+					<NavLink to="/search" className={setActive}>
+						Найти еду
+					</NavLink>
+					<NavLink to="/basket" className={setActive}>
+						Заказы
+					</NavLink>
+					<NavLink to="/profile" className={setActive}>
+						Профиль
+					</NavLink>
 				</Nav>
 				<Nav className="ml-auto">
 					<Button variant={"outline-light"}>Авторизация</Button>
--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import Search from "../pages/Search";
+import Basket from "../pages/Basket";
+import Profile from "../pages/Profile";
 
 function AppRouter() {
 	return (
 		<Routes>
 			<Route path="/" element={<Main />} />
 			<Route path="/search" element={<Search />} />
+			<Route path="/basket" element={<Basket />} />
+			<Route path="/profile" element={<Profile />} />
 		</Routes>
 	);
 }
